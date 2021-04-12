2c00a1d989c80402f4a1c1c0e66473345c30ac2a
TheProgrammingArchive <55741209+TheProgrammingArchive@users.noreply.github.com>
2021-04-12 15:34:30 +0530

    Add files via upload

---
+++ rickroll-detector/installer.py
@@ -0,0 +1,19 @@
+import requests
+import zipfile
+import platform
+import sys
+
+chrome_version = '89.0.4389.23'
+
+def install_chrome_driver_package(link):
+    chromedriver_content = requests.get(link)
+
+    chromedriver_file = open('Chromedriver.zip', 'wb')
+    chromedriver_file.write(chromedriver_content.content)
+    chromedriver_file.close()
+
+    with zipfile.ZipFile('Chromedriver.zip') as zip_file:
+        zip_file.extractall('ChromeDriver')
+
+if __name__ == '__main__':
+    install_chrome_driver_package(f'https://chromedriver.storage.googleapis.com/{chrome_version}/chromedriver_win32.zip')
